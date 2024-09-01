70c213e79cff6273a40348110074ae036a09cec4
Doszhan <doszhan.work@gmail.com>
2024-09-01 15:35:20 +0500

    fix:zsh script

--- Linux/install_zsh.sh
+++ Linux/install_zsh.sh
@@ -5,10 +5,13 @@ sudo apt update
 sudo apt -y install zsh curl git
 
 # Установка Oh My Zsh
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+yes | sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# Определение переменной ZSH_CUSTOM
+ZSH_CUSTOM=${ZSH_CUSTOM:-~/.oh-my-zsh/custom}
 
 # Установка плагинов zsh-autosuggestions и zsh-syntax-highlighting
-git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
 
 # Настройка .zshrc: добавление темы и плагинов
